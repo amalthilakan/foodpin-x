f3822aea6c7b5583fee81abe97844c129a907ba3
amalthilakan <amalthilakan111@gmail.com>
2025-11-26 13:51:37 +0530

    fix/ google maps api key

---
+++ app.config.ts
@@ -0,0 +1,65 @@
+import { ConfigContext, ExpoConfig } from 'expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+    ...config,
+    name: "foodpin",
+    slug: "foodpin",
+    version: "1.0.2",
+    orientation: "portrait",
+    icon: "./assets/images/foodpinlogo.png",
+    scheme: "foodpin",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+        supportsTablet: true,
+        config: {
+            googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+        }
+    },
+    android: {
+        adaptiveIcon: {
+            backgroundColor: "#E6F4FE",
+            foregroundImage: "./assets/images/foodpinlogo.png",
+            backgroundImage: "./assets/images/android-icon-background.png",
+            monochromeImage: "./assets/images/android-icon-monochrome.png"
+        },
+        edgeToEdgeEnabled: true,
+        predictiveBackGestureEnabled: false,
+        package: "com.amalthilakan.foodpin",
+        config: {
+            googleMaps: {
+                apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+            }
+        }
+    },
+    web: {
+        output: "static",
+        favicon: "./assets/images/foodpinlogo.png"
+    },
+    plugins: [
+        "expo-router",
+        [
+            "expo-splash-screen",
+            {
+                "image": "./assets/images/splash.png",
+                "imageWidth": 200,
+                "resizeMode": "contain",
+                "backgroundColor": "#ffffff",
+                "dark": {
+                    "backgroundColor": "#000000"
+                }
+            }
+        ]
+    ],
+    experiments: {
+        typedRoutes: true,
+        reactCompiler: true
+    },
+    extra: {
+        router: {},
+        eas: {
+            projectId: "f1f2e682-5038-4ce3-ad89-b5aef44da52d"
+        }
+    },
+    owner: "amalthilakan"
+});
